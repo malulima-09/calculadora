1af7fa10932c6852fa4b9aa17b54d495865b2985
Maria Luiza <limamarialuiza928@gmail.com>
2026-09-17 16:33:37 -0300

    calculadora

--- cypress/e2e/calc.cy.ts
+++ cypress/e2e/calc.cy.ts
@@ -1,13 +1,33 @@
-describe ('Calculadora', () => {
+describe('Calculadora', () => {
   beforeEach(() => {
     cy.visit("http://localhost:3000").wait(1000);
   });
 
-  it("Soma Correta", () => {
+  it("Soma correta", () => {
     cy.get('[data-cy="num1"').type("10").wait(1000);
     cy.get('[data-cy="num2"').type("5").wait(1000);
     cy.get('[data-cy="add"').click().wait(1000);
-    cy.get('[data-cy="result"').should("contain","15");
-    
-  })
-})
+    cy.get('[data-cy="result"]').should("contain", "15");
+  });
+
+  it("Subtração correta", () => {
+    cy.get('[data-cy="num1"]').type("10").wait(1000);
+    cy.get('[data-cy="num2"]').type("5").wait(1000);
+    cy.get('[data-cy="sub"]').click().wait(1000);
+    cy.get('[data-cy="result"]').should("contain", "5");
+  });
+
+  it("Multiplicação correta", () => {
+    cy.get('[data-cy="num1"]').type("10").wait(1000);
+    cy.get('[data-cy="num2"]').type("5").wait(1000);
+    cy.get('[data-cy="mul"]').click().wait(1000);
+    cy.get('[data-cy="result"]').should("contain", "50");
+  });
+
+  it("Divisão correta", () => {
+    cy.get('[data-cy="num1"]').type("10").wait(1000);
+    cy.get('[data-cy="num2"]').type("5").wait(1000);
+    cy.get('[data-cy="div"]').click().wait(1000);
+    cy.get('[data-cy="result"]').should("contain", "2");
+  });
+});
